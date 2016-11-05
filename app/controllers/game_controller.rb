e345f5688063b1a8d1bf407365f4360204310f63
Ha Lee <halee20@users.noreply.github.com>
2016-11-05 00:38:47 -0500

    1
    1

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,41 @@
+class GameController < ApplicationController
+  def user_plays_rock
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    elsif @computer_move == "scissors"
+      @outcome = "won"
+    end
+
+    render ("user_plays_rock.html.erb")
+  end
+
+  def user_plays_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    end
+    render ("user_plays_paper.html.erb")
+  end
+
+  def user_plays_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    elsif @computer_move == "scissors"
+      @outcome = "tied"
+    end
+    render ("user_plays_scissors.html.erb")
+  end
+end
